dc7989732b6e51b515f3ae6907e3803d436fcc58
ninglz <ninglz@yeah.net>
2023-03-24 14:48:13 +0800

    update

--- pages/index/_components/Footer.tsx
+++ pages/index/_components/Footer.tsx
@@ -5,11 +5,10 @@ export default () => {
         Ning © 2023
       </p>
       
-      <div class="text-sm text-gray flex flex-nowra items-center inline-block mt-1">
-        <a href="http://beian.miit.gov.cn/" target="_blank" rel="nofollow noopener">蜀ICP备20010142号</a>
-        &nbsp;|&nbsp; <img src="./police_beian.png" alt="police-icon"/>
-        <a class='flex flex-nowrap' href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=51019002002735" rel="nofollow noopener" target="_blank">
-        川公网安备 51019002002735号
+      <div class="text-sm text-gray  mt-1">
+        <a class="inline-block" href="http://beian.miit.gov.cn/" target="_blank" rel="nofollow noopener">蜀ICP备20010142号</a>&nbsp;|&nbsp;
+        <a class='inline-block' href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=51019002002735" rel="nofollow noopener" target="_blank">
+         <img class='inline-block' src="./police_beian.png" alt="police-icon"/>川公网安备 51019002002735号
         </a>
       </div>
     </footer>
